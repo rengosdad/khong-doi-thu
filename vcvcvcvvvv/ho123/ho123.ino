83b71ce71e09fd1c32016451601ce225e5ac59d5
rengokusad <113831923+rengosdad@users.noreply.github.com>
2023-03-15 06:49:22 +0700

    Update ho123.ino

--- vcvcvcvvvv/ho123/ho123.ino
+++ vcvcvcvvvv/ho123/ho123.ino
@@ -58,35 +58,35 @@ char blt=0;                                             //int khach;    /ON/OF /
                                               //pinMode(CC,OUTPUT);//BƠM NƯỚC
                                               //pinMode(DD,OUTPUT);//NÂNG HẠ( STEP motor)
 ////////////////////////////////////////////////////////////////////////////////////////////                                              
- #include <SPI.h>
-#include <MFRC522.h>
+//#include <SPI.h>
+//#include <MFRC522.h>
 
-#define RST_PIN         46
-#define SS_PIN          53
+//#define RST_PIN         46
+//#define SS_PIN          53
 
-int UID[4], i;
+//int UID[4], i;
 
-int ID1[4] = {234, 95, 235, 173 }; //Thẻ bật tắt đèn
-int sang = 17;
+//int ID1[4] = {234, 95, 235, 173 }; //Thẻ bật tắt đèn
+//int sang = 17;
 
-int dem = 0;
+//int dem = 0;
  
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+//MFRC522 mfrc522(SS_PIN, RST_PIN);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 void setup() {
-  {
+  //{
 
-    Serial.begin(9600);   
+   // Serial.begin(9600);   
 
-    pinMode(sang, OUTPUT);
-    digitalWrite(sang, LOW);
+    //pinMode(sang, OUTPUT);
+   // digitalWrite(sang, LOW);
     
-    SPI.begin();    
-    mfrc522.PCD_Init();
-
-}
+   // SPI.begin();    
+ //   mfrc522.PCD_Init();
+//
+//}
 
  
  Serial.begin(9600); 
@@ -318,55 +318,55 @@ myStepper.step(2 * stepsPerRevolution);
                                                      //             digitalWrite(DD,LOW);     //Chạy Step -
  
                                                     //        }
-  {
+  //{
 
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  { 
-    return; 
-  }
+//  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+ // { 
+   // return; 
+ // }
   
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {  
-    return;  
-  }
+//  if ( ! mfrc522.PICC_ReadCardSerial()) 
+//  {  
+    //return;  
+ // }
   
-  Serial.print("UID của thẻ: ");   
+ // Serial.print("UID của thẻ: ");   
   
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  { 
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");   
-    UID[i] = mfrc522.uid.uidByte[i];
-    Serial.print(UID[i]);
-  }
-
-  Serial.println("   ");
+ // for (byte i = 0; i < mfrc522.uid.size; i++) 
+///  { 
+  //  Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");   
+   // UID[i] = mfrc522.uid.uidByte[i];
+  //  Serial.print(UID[i]);
+  //}
+//
+ // Serial.println("   ");
   
-  if (UID[i] == ID1[i])
-  {
-    dem ++;
-    Serial.print("Biến Đếm: ");
-    Serial.println(dem);
+ // if (UID[i] == ID1[i])
+//  {
+    //dem ++;
+   // Serial.print("Biến Đếm: ");
+   // Serial.println(dem);
     
-      if ( (dem % 2) == 1) //Số lẻ đèn ON
-      {
-        digitalWrite(sang, HIGH);
-        Serial.println("ĐÈN ON");    
-      }
-      else
-      {
-        digitalWrite(sang, LOW);
-        Serial.println("ĐÈN OFF");       
-      }
-  }
-  
-  else
-  {
-    Serial.println("SAI THẺ........");
-  }
+    //  if ( (dem % 2) == 1) //Số lẻ đèn ON
+   //   {
+      //  digitalWrite(sang, HIGH);
+      //  Serial.println("ĐÈN ON");    
+     // }
+    //  else
+    //  {
+      //  digitalWrite(sang, LOW);
+      //  Serial.println("ĐÈN OFF");       
+ //     }
+//  }
+ // 
+ // else
+//  {
+  //  Serial.println("SAI THẺ........");
+ // }
 
-  mfrc522.PICC_HaltA();  
-  mfrc522.PCD_StopCrypto1();
+ // mfrc522.PICC_HaltA();  
+//  mfrc522.PCD_StopCrypto1();
 
-}
+//}
   delay(1000);
 }
